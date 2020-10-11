a9b40439c5ccbe411474971bf76da7184f5883f1
colkey <colinakey@yahoo.co.jp>
2020-10-11 22:21:30 +0900

    initial commit

---
+++ owncam/__main__.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+def main():
+    capture = cv2.VideoCapture(0)
+
+    while(True):
+        ret, frame = capture.read()
+        cv2.imshow('ownCam', frame)
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
