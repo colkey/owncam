4deb82acf9edec0fb4d1e0d14b62f1d90d88fd01
colkey <colinakey@yahoo.co.jp>
2020-10-18 00:47:07 +0900

    update: 画像処理部を関数に切り出し

--- owncam/__main__.py
+++ owncam/__main__.py
@@ -8,13 +8,22 @@ def main():
     capture = cv2.VideoCapture(config['DEVICE_ID'], cv2.CAP_DSHOW)
 
     while(capture.isOpened()):
-        ret, frame = capture.read()
-        cv2.imshow('ownCam', frame)
+        ret, frame = processor(*capture.read())
+        if ret:
+            cv2.imshow('ownCam', frame)
+
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
 
     capture.release()
     cv2.destroyAllWindows()
+
+
+def processor(ret, frame):
+    if not ret:
+        return ret, frame
+
+    return ret, frame
 
 
 if __name__ == "__main__":
